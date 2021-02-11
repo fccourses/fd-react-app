3bd6443e9bcabc26935aa37d7459443ca0b21678
Vladimir <freshcode.yt@gmail.com>
2021-02-11 14:58:52 +0200

    Refactor AlohaDashboard

--- src/components/AlohaDashboard/index.jsx
+++ src/components/AlohaDashboard/index.jsx
@@ -2,25 +2,16 @@ import { Component } from 'react';
 import Aloha from '../Aloha';
 
 class AlohaDashboard extends Component {
+  mapAloha = (user) => (
+    <li>
+      <Aloha name={`${user.firstName} ${user.lastName}`} />
+    </li>
+  );
+
   render() {
     const { users } = this.props;
 
-    const liArray = users.map((user) => {
-      return (
-        <li>
-          <Aloha name={`${user.firstName} ${user.lastName}`}>
-            <div>
-              <span>{user.id}</span>
-            </div>
-            <p>
-              <span>{user.id}</span>
-            </p>
-          </Aloha>
-        </li>
-      );
-    });
-
-    return <ul>{liArray}</ul>;
+    return <ul>{users.map(this.mapAloha)}</ul>;
   }
 }
 export default AlohaDashboard;
