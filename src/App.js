66fc7be60843fcb2f546e2280a06e254b838fd6f
Vladimir <freshcode.yt@gmail.com>
2021-02-11 16:15:26 +0200

    Add more example Dashboard

--- src/App.js
+++ src/App.js
@@ -3,63 +3,10 @@ import './App.css';
 import AlohaDashboard from './components/AlohaDashboard';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [
-        {
-          id: 1,
-          firstName: 'John',
-          lastName: 'Doe',
-        },
-        {
-          id: 2,
-          firstName: 'Test',
-          lastName: 'Doe',
-        },
-        {
-          id: 3,
-          firstName: 'Masha',
-          lastName: 'Doe',
-        },
-        {
-          id: 4,
-          firstName: 'Sasha',
-          lastName: 'Doe',
-        },
-        {
-          id: 5,
-          firstName: 'Dasha',
-          lastName: 'Doe',
-        },
-      ],
-      isDirectOrder: true,
-    };
-  }
-
-  sortUsers = () => {
-    const { users, isDirectOrder } = this.state;
-    // const usersCopy = [...users];
-    const usersCopy = JSON.parse(JSON.stringify(users));
-    this.setState({
-      isDirectOrder: !isDirectOrder,
-      users: usersCopy.sort((a, b) => {
-        if (isDirectOrder) {
-          return b.id - a.id;
-        }
-        return a.id - b.id;
-      }),
-    });
-  };
-
   render() {
-    const { users, isDirectOrder } = this.state;
-
     return (
       <>
-        Порядок сортировки по id: {isDirectOrder?'Прямой': 'Реверс'}
-        <AlohaDashboard users={users} />
-        <button onClick={this.sortUsers}>SORT</button>
+        <AlohaDashboard />
       </>
     );
   }
--- src/components/AlohaDashboard/index.jsx
+++ src/components/AlohaDashboard/index.jsx
@@ -2,16 +2,67 @@ import { Fragment, Component } from 'react';
 import Aloha from '../Aloha';
 
 class AlohaDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          id: 1,
+          firstName: 'John',
+          lastName: 'Doe',
+        },
+        {
+          id: 2,
+          firstName: 'Test',
+          lastName: 'Doe',
+        },
+        {
+          id: 3,
+          firstName: 'Masha',
+          lastName: 'Doe',
+        },
+        {
+          id: 4,
+          firstName: 'Sasha',
+          lastName: 'Doe',
+        },
+        {
+          id: 5,
+          firstName: 'Dasha',
+          lastName: 'Doe',
+        },
+      ],
+      isDirectOrder: true,
+    };
+  }
+  sortUsers = () => {
+    const { users, isDirectOrder } = this.state;
+    // const usersCopy = [...users];
+    const usersCopy = JSON.parse(JSON.stringify(users));
+    this.setState({
+      isDirectOrder: !isDirectOrder,
+      users: usersCopy.sort((a, b) => {
+        if (isDirectOrder) {
+          return b.id - a.id;
+        }
+        return a.id - b.id;
+      }),
+    });
+  };
   mapAloha = (user) => (
-    <Fragment key={user.id}>
-      <Aloha name={`${user.firstName} ${user.lastName}`} />
-    </Fragment>
+    <Aloha key={user.id} name={`${user.firstName} ${user.lastName}`} />
   );
 
   render() {
-    const { users } = this.props;
+    const { users, isDirectOrder } = this.state;
 
-    return <div>{users.map(this.mapAloha)}</div>;
+    return (
+      <div>
+        Порядок сортировки по id: {isDirectOrder ? 'Прямой' : 'Реверс'}
+        {users.map(this.mapAloha)}
+        <button onClick={this.sortUsers}>SORT</button>
+      </div>
+    );
   }
 }
 
