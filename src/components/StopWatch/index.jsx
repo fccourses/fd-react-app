7df0beb31f1bde84c69fd1717f5ed515b4e8c608
Vladimir <freshcode.yt@gmail.com>
2021-02-12 16:16:49 +0200

    Add some functional

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -7,7 +7,6 @@ class StopWatch extends Component {
     this.state = {
       count: 0,
     };
-    console.log('constructor');
   }
 
   tick = () => {
@@ -15,35 +14,64 @@ class StopWatch extends Component {
   };
 
   start = () => {
-    this.intervalId = setInterval(this.tick, 1000);
+    if (!this.intervalId) {
+      this.intervalId = setInterval(this.tick, 1000);
+    }
+  };
+
+  stop = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  };
+
+  reset = () => {
+    this.stop();
+    this.setState({ count: 0 });
   };
   /* 
     AFTER FIRST RENDER
   */
   componentDidMount() {
-    console.log('componentDidMount');
     this.start();
   }
 
   /* 
     AFTER EACH RENDER
   */
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
+  componentDidUpdate() {}
 
   /* 
     BEFORE UNMOUNT
   */
   componentWillUnmount() {
-    clearInterval(this.intervalId);
+    this.stop();
   }
 
   render() {
-    console.log('render');
     return (
       <article>
         <h1>{this.state.count}</h1>
+        <button
+          onClick={() => {
+            this.start();
+          }}
+        >
+          START
+        </button>
+        <button
+          onClick={() => {
+            this.stop();
+          }}
+        >
+          STOP
+        </button>
+        <button
+          onClick={() => {
+            this.reset();
+          }}
+        >
+          RESET
+        </button>
       </article>
     );
   }
