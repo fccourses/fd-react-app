79cd5ab380d9a052ce1fc404bb0e752f7102ae14
Vladimir <freshcode.yt@gmail.com>
2021-02-18 16:43:06 +0200

    Add SelectedUserList PropTypes

--- src/components/SelectedUserList/index.jsx
+++ src/components/SelectedUserList/index.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { userPropType } from './../UserList/UserCard';
 
-const SelectedUserList = (props) => {
-  const { users=[] } = props;
-  const filteredUsers = users.filter((u) => u.isSelected);
+const SelectedUserList = props => {
+  const { users } = props;
+  const filteredUsers = users.filter(u => u.isSelected);
 
   return (
     <div>
-      {filteredUsers.map((user) => (
+      {filteredUsers.map(user => (
         <span>
           {user.firstName} {user.lastName}
         </span>
       ))}
     </div>
   );
+};
+
+SelectedUserList.defaultProps = {
+  users: [],
+};
+
+SelectedUserList.propTypes = {
+  users: PropTypes.arrayOf(userPropType),
 };
 
 export default SelectedUserList;
