142fb07ebd486baba847d38c4d96709f5f3ef504
Vladimir <freshcode.yt@gmail.com>
2021-02-19 16:14:38 +0200

    Add props drilling

--- src/App.js
+++ src/App.js
@@ -10,33 +10,20 @@ import NamedList from './components/NamedList';
 // import Calendar from './components/Calendar';
 
 const App = props => {
-  const numbers = [1, 2];
-
-  const renderListItems = () => {
-    return numbers.map((currentNumber, index, numbersArray) => {
-      return (
-        <div key={`${currentNumber}-${index}`} title='test li'>
-          {currentNumber} {index}
-        </div>
-      );
-    });
-  };
-
   return (
     <>
-      {/*     <NamedList name='TEST LIST FROM APP'>
-        <p>p 1</p>
-        <div>DIV 2</div>
-        <div>DIV 3</div>
-      </NamedList> */}
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <ImageWrapper width='500px' height='500px'>
-          <img
-            src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
-            alt=''
-          />
-        </ImageWrapper>
-      </div>
+      <ImageWrapper
+        width='500px'
+        height='500px'
+        style={{border: '2px solid red'}}
+        onClick={alert}
+        title='test'
+      >
+        <img
+          src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+          alt=''
+        />
+      </ImageWrapper>
     </>
   );
 };
--- src/components/ImageWrapper/index.jsx
+++ src/components/ImageWrapper/index.jsx
@@ -3,24 +3,26 @@ import PropTypes from 'prop-types';
 import styles from './ImageWrapper.module.scss';
 
 const ImageWrapper = props => {
-  const { children, width, height } = props;
+  const { width, height, style, ...restProps } = props;
 
   const inlineStyles = {
+    ...style,
     width,
     height,
   };
 
-  return (
-    <div style={inlineStyles} className={styles.wrapper}>
-      {children}
-    </div>
-  );
+  return <div style={inlineStyles} {...restProps} />;
+};
+
+ImageWrapper.defaultProps = {
+  className: styles.wrapper,
 };
 
 ImageWrapper.propTypes = {
   children: PropTypes.element.isRequired,
   width: PropTypes.string.isRequired,
   height: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 
 export default ImageWrapper;
