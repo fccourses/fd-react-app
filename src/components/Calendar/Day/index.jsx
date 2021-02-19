ef784a152cf074db34259edaa2ad519abd53b9d4
Vladimir <freshcode.yt@gmail.com>
2021-02-19 16:33:27 +0200

    Add Day

--- src/components/Calendar/Day/index.jsx
+++ src/components/Calendar/Day/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { format } from 'date-fns';
+import { format, isToday, isThisMonth, isSameMonth } from 'date-fns';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import styles from './Day.module.scss';
 
 const Day = props => {
-  const { day } = props;
-  return <td>{format(day, 'd')}</td>;
+  const { day, currentDay } = props;
+
+  const classNames = cx(styles.day, {
+    [styles.currentMonthDay]: isSameMonth(day, currentDay),
+    [styles.currentDay]: isToday(day),
+  });
+
+  return <td className={classNames}>{format(day, 'd')}</td>;
+};
+
+Day.propTypes = {
+  day: PropTypes.instanceOf(Date).isRequired,
+  currentDay: PropTypes.instanceOf(Date).isRequired,
 };
 
 export default Day;
