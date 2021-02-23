a53fb7e37420416e208a98517291f480efc8f654
Vladimir <freshcode.yt@gmail.com>
2021-02-23 16:48:19 +0200

    Add routes to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 // import ImageWrapper from './components/ImageWrapper';
 // import NamedList from './components/NamedList';
 // import SignInForm from './components/SignInForm';
@@ -8,15 +9,66 @@ import React from 'react';
 // import UserCard from './components/UserList/UserCard';
 // import Counter from './components/Counter';
 // import Calendar from './components/Calendar';
-// import UsersLoader from './components/UsersLoader';
+import UsersLoader from './components/UsersLoader';
 import WindowSizes from './components/WindowSizes';
 
 const App = props => {
   return (
-    <>
-      <WindowSizes />
-    </>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/contacts'>Contacts</Link>
+          </li>
+          <li>
+            <Link to='/users-loader'>User Loader</Link>
+          </li>
+          <li>
+            <Link to='/window'>Window Sizes</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/contacts'>
+          <Contacts />
+        </Route>
+
+        <Route path='/users-loader'>
+          <UsersLoader />
+        </Route>
+
+        <Route path='/about'>
+          <About />
+        </Route>
+
+        <Route path='/window'>
+          <WindowSizes />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
+};
+
+const Home = () => {
+  return <div>HOME PAGE</div>;
+};
+
+const Contacts = () => {
+  return <div>Contacts PAGE</div>;
+};
+
+const About = () => {
+  return <div>About PAGE</div>;
 };
 
 export default App;
